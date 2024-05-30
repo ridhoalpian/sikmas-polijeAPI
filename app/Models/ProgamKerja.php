9981bc79d12af8000f50724a36a6eab652acd802
itsdrsw <marswan27@gmail.com>
2024-05-30 13:57:28 +0700

    [MODEL] make model progam kerja
    Membuat model progam kerja sekaligus migration dan seed database

---
+++ app/Models/ProgamKerja.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProgamKerja extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'idproker';
+
+    protected $table = 'progam_kerja';
+
+    protected $guarded = ['idproker'];
+
+    protected $fillable = [
+        'status_proker',
+        'lampiran_proker',
+        'nama_proker',
+        'penanggung_jawab',
+        'uraian_proker',
+        'periode',
+        'user_id'
+    ];
+}
