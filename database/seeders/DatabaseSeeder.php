144b4d4f2d8b626c9f273a2612d3f609b77b76e9
itsdrsw <marswan27@gmail.com>
2024-05-30 14:22:38 +0700

    [CONTROLLER] make controller kegitan
    Membuat controller kegiatan dan menambahkan pemanggilan seeder untuk tabel kegiatan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProkerSeeder::class,
+            KegiatanSeeder::class,
         ]);
     }
 }
